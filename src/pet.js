2d6c18af38f8d8c023855a565769d1a2302d2144
Szabina Kovacs <szabinkov@gmail.com>
2020-05-13 15:15:26 +0100

    made them age

--- src/pet.js
+++ src/pet.js
@@ -1,5 +1,9 @@
-function Pet(name) {
+function Pet(name,age) {
     this.name = name;
+    this.age = 0;
+    this.growUp = function() {
+        this.age += 1;
+    };
 }
 
 module.exports = Pet;
--- tests/pet.test.js
+++ tests/pet.test.js
@@ -1,7 +1,19 @@
 const Pet = require('../src/pet');
 
 describe('constructor', () => {
-  it('returns an object', () => {
-    expect(new Pet('Fido')).toBeInstanceOf(Object);
+  it('sets the name property', () => {
+    const pet = new Pet('Fido');
+
+    expect(pet.name).toEqual('Fido');
+    expect(pet.age).toEqual(0);
+  });
+});
+describe('growUp', () => {
+  it('increments the age by 1', () => {
+    const pet = new Pet('Fido');
+
+    pet.growUp();
+
+    expect(pet.age).toEqual(1);
   });
 });
